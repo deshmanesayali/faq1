2661cc483262a5b41eff0012c304df04924fa8e7
SD <sd796@njit.edu>
2018-12-13 03:29:48 -0500

    Task: created a test to check for successful login

--- tests/Browser/SuccessfulLoginTest.php
+++ tests/Browser/SuccessfulLoginTest.php
@@ -2,22 +2,32 @@
 
 namespace Tests\Browser;
 
+use App\User;
 use Tests\DuskTestCase;
-use Laravel\Dusk\Browser;
+use Laravel\Dusk\Chrome;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
-class SuccessfulLoginTest extends DuskTestCase
+class ExampleTest extends DuskTestCase
 {
+    use DatabaseMigrations;
+
     /**
-     * A Dusk test example.
+     * A basic browser test example.
      *
      * @return void
      */
-    public function testExample()
+    public function testSuccessfulLogin()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+        $user = factory(User::class)->create([
+            'email' => 'taylor@laravel.com',
+        ]);
+
+        $this->browse(function ($browser) use ($user) {
+            $browser->visit('/login')
+                ->type('email', $user->email)
+                ->type('password', 'secret')
+                ->press('Login')
+                ->assertPathIs('/home');
         });
     }
 }
